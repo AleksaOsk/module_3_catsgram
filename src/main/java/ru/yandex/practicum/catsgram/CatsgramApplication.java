5d36cd0d544eb4d21de46d8db804dec424c90db5
Aleksa sanyaaa_a <holmesSUQA@yandex.ru>
2024-11-16 23:11:45 +0300

    ывса\ывсафы

--- src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
@@ -3,9 +3,15 @@ package ru.yandex.practicum.catsgram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @SpringBootApplication
 public class CatsgramApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(CatsgramApplication.class, args);
     }
 }
